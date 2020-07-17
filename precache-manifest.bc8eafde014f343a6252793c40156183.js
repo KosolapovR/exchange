c4dcfb572ee41eb12cfd32edcfd64ba7130a4382
$(KosolapovR) <$(KosolapovR)@users.noreply.github.com>
2020-07-17 13:24:39 +0300

    Updates

---
+++ precache-manifest.bc8eafde014f343a6252793c40156183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03124e2e4c8e13378e8c986a95116ce7",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "4e9bebc5184a0c11add3",
+    "url": "/exchange/static/css/main.09ae55fb.chunk.css"
+  },
+  {
+    "revision": "cf329fb473366f9d0f54",
+    "url": "/exchange/static/js/2.346030b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/exchange/static/js/2.346030b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9bebc5184a0c11add3",
+    "url": "/exchange/static/js/main.49694125.chunk.js"
+  },
+  {
+    "revision": "6ae5ba7cac29f5dd1942",
+    "url": "/exchange/static/js/runtime-main.8e986161.js"
+  }
+]);
